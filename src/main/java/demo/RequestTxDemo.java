c1edee9717ec55d718f850ec5b030fde70b0cbc1
GinMu <gin.musheng@gmail.com>
2020-04-26 15:46:56 +0800

    request tx demo

---
+++ src/main/java/demo/RequestTxDemo.java
@@ -0,0 +1,17 @@
+package demo;
+
+
+import com.blink.jtblc.client.Remote;
+import com.blink.jtblc.client.bean.Account;
+import com.blink.jtblc.connection.Connection;
+import com.blink.jtblc.connection.ConnectionFactory;
+
+public class RequestTxDemo {
+	public static void main(String[] args) throws Exception {
+		Connection conn = ConnectionFactory.getCollection("wss://hc.jingtum.com:5020/");
+		Remote remote = new Remote(conn, true);
+		String hash = "7D8554BE9EEA1AC7CA9D4DD437D27A5BEB0339A7E538907CA69EEF9674F8A172";
+		Account bean = remote.requestTx(hash);
+		Connection.close();
+	}
+}
